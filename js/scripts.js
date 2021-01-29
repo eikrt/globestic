c2f1d2d9308aa0904bb375b3c30e6690b4638640
Eino Korte <e.i.korte@gmail.com>
2021-01-29 19:47:22 +0200

    now a rotating earth, next controls

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,22 @@ document.body.appendChild( renderer.domElement );
 
 function animate() {
     requestAnimationFrame( animate );
-    globe.rotation.x += 0.01;
+     globe.rotation.x += 0.01;
     globe.rotation.y += 0.01;
     renderer.render( scene, camera );
 };
 function init() {
     
 };
-const geometry = new THREE.SphereGeometry(2, 64, 64);
-const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+const geometry = new THREE.SphereGeometry(2, 32, 32);
+const material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('../res/globe.jpg') } );
 const globe = new THREE.Mesh( geometry, material );
+
+scene.add(new THREE.AmbientLight(0x333333));
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5,3,5);
+scene.add(light);
+
 scene.add(globe);
 camera.position.z = 5;
 
